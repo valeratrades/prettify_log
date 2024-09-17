0a2fd50873a0cc63cf50e1c3ff57956bf8723cd4
valeratrades <v79166789533@gmail.com>
2024-09-17 18:55:58 +0000

    _

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@
 use ast::tokens_into_ast;
 use clap::{command, Parser};
 use clap_stdin::MaybeStdin;
+use color_eyre::eyre::Result;
 use lexer::str_into_tokens;
+use tree_kinds::TreeKind;
 
 mod ast;
 mod lexer;
@@ -16,14 +18,50 @@ pub mod utils;
 #[command(author, version, about, long_about = None)]
 struct Cli {
 	s: MaybeStdin<String>,
+
+	/// Allow parsing structures like `explanation nesting level: key: value`
+	///NB: Adds `n/2` for each encountered colon (could be implemented better, but eh)
+	/// # Ex
+	/// ```sh
+	/// some explanation: another explanation: key: value""" | prettify_log - --maybe-colon-nested
+	/// ```
+	#[arg(long)]
+	maybe_colon_nested: bool,
 }
 
 fn main() {
 	color_eyre::install().unwrap();
-	let args = Cli::parse();
-	let input = format!("{}", args.s);
+	let cli = Cli::parse();
+	let input = format!("{}", cli.s);
 
-	let tokens = str_into_tokens(input).unwrap();
-	let parsed = tokens_into_ast(tokens).unwrap();
+	let parsed = match parse(input, cli.maybe_colon_nested) {
+		Ok(ast) => ast,
+		Err(e) => {
+			eprintln!("Error: {e}");
+			std::process::exit(1);
+		}
+	};
 	println!("{parsed}");
+}
+
+fn parse(input: String, maybe_colon_nested: bool) -> Result<TreeKind> {
+	let mut tokens = str_into_tokens(input)?;
+	let ast = match maybe_colon_nested {
+		#[allow(clippy::never_loop)]
+		true => loop {
+			match tokens_into_ast(tokens.clone()) {
+				Ok(ast) => break ast,
+				Err(e) => {
+					let i = tokens.iter().position(|t| matches!(t, lexer::Token::InnerDelim));
+					if let Some(i) = i {
+						tokens = tokens[i + 1..].to_vec();
+					} else {
+						return Err(e);
+					}
+				}
+			}
+		},
+		false => tokens_into_ast(tokens)?,
+	};
+	Ok(ast)
 }
